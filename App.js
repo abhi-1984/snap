8266c5677125f1ce039583a9dbc410813a4d0b34
Abhi <abhishekkambli3@gmail.com>
2019-04-14 17:47:20 +0530

    face and general model done

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  StyleSheet,
+  FlatList,
   Text,
   View,
   StatusBar,
@@ -22,7 +22,10 @@ export default class App extends React.Component {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
     activeModelName: "Basic",
-    predictions: []
+    predictions: [],
+    isPredictionsViewVisible: false,
+    randomColor: {},
+    faceDetection: []
   };
 
   async componentWillMount() {
@@ -65,26 +68,75 @@ export default class App extends React.Component {
     return manipulatedImage.base64;
   };
 
-  predict = async image => {
-    let predictions = await clarifai.models.predict(
-      Clarifai.GENERAL_MODEL,
-      image
-    );
-    return predictions;
+  predict = async (image, modelName) => {
+    if (modelName == "Basic") {
+      let predictions = await clarifai.models.predict(
+        Clarifai.GENERAL_MODEL,
+        image
+      );
+      return predictions;
+    } else if (modelName == "Face") {
+      let predictions = await clarifai.models.predict(
+        "c0c0ac362b03416da06ab3fa36fb58e3",
+        image
+      );
+      return predictions;
+    }
   };
 
-  objectDetection = async () => {
+  objectDetection = async modelName => {
     let photo = await this.capturePhoto();
     let resized = await this.resize(photo);
-    let predictions = await this.predict(resized);
+    let predictions = await this.predict(resized, modelName);
+    let iconColors = [
+      {
+        bgColor: "#CAEEF9",
+        foreGroundColor: "#39AFD5"
+      },
+      {
+        bgColor: "#DDD5F0",
+        foreGroundColor: "#7B64C0"
+      },
+      {
+        bgColor: "#FBCCD2",
+        foreGroundColor: "#F35369"
+      },
+      {
+        bgColor: "#FBCCEB",
+        foreGroundColor: "#FF4DC2"
+      },
+      {
+        bgColor: "#BDDDFF",
+        foreGroundColor: "#0052CC"
+      }
+    ];
+
     this.setState({
-      predictions: predictions.outputs[0].data.concepts
+      predictions:
+        modelName == "Basic"
+          ? predictions.outputs[0].data.concepts
+          : predictions.outputs[0].data.regions[0],
+      isPredictionsViewVisible: true,
+      randomColor: iconColors[Math.floor(Math.random() * iconColors.length)]
     });
-    console.log("predictions are -> ", this.state.predictions);
+    console.log("Predcitions are>>>>>", this.state.predictions);
   };
 
+  closeDialog() {
+    this.setState({
+      isPredictionsViewVisible: false
+    });
+  }
+
   render() {
-    const { hasCameraPermission, activeModelName, predictions } = this.state;
+    const {
+      hasCameraPermission,
+      activeModelName,
+      predictions,
+      isPredictionsViewVisible,
+      randomColor
+    } = this.state;
+
     if (hasCameraPermission === null) {
       return <View style={{ backgroundColor: "#fff" }} />;
     } else if (hasCameraPermission === false) {
@@ -93,106 +145,196 @@ export default class App extends React.Component {
       return (
         this.state.fontLoaded && (
           <View style={styles.wrapper}>
-            <StatusBar barStyle="light-content" />
-            <View style={styles.header}>
-              <Text style={styles.appName}>
-                snap
-                <Text style={styles.muted}>.ai</Text>
-              </Text>
-              <TouchableOpacity
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back
-                  });
-                }}
-              >
-                <Icon.Ionicons name="ios-repeat" size={24} color="#fff" />
-              </TouchableOpacity>
-            </View>
-
-            <View style={styles.cameraViewWrapper}>
-              <View style={styles.cameraBorderView}>
-                <Camera
-                  style={styles.cameraView}
-                  ref={ref => {
-                    this.camera = ref;
+            <StatusBar hidden={true} />
+            <View style={styles.homeView}>
+              <View style={styles.header}>
+                <Text style={styles.appName}>
+                  snap
+                  <Text style={styles.muted}>.ai</Text>
+                </Text>
+                <TouchableOpacity
+                  style={styles.switchCameraViewBG}
+                  onPress={() => {
+                    this.setState({
+                      type:
+                        this.state.type === Camera.Constants.Type.back
+                          ? Camera.Constants.Type.front
+                          : Camera.Constants.Type.back
+                    });
                   }}
-                  type={this.state.type}
-                />
+                >
+                  <Icon.Ionicons name="ios-repeat" size={24} color="#fff" />
+                </TouchableOpacity>
               </View>
-              <Text style={styles.helperText}>Snap your photo</Text>
-            </View>
-            {console.log("activeModelName ->", activeModelName)}
 
-            <View style={styles.modelBarView}>
-              <TouchableOpacity
-                style={[styles.modelButtonView, styles.shiftedButtonView]}
-                onPress={() => this.modelButtonPressed("Basic")}
-              >
-                <View
-                  style={
-                    activeModelName == "Basic"
-                      ? [styles.modelIconBG, styles.activeModelView]
-                      : styles.modelIconBG
-                  }
-                >
-                  <Icon.Ionicons name="ios-apps" size={28} color="#000" />
-                </View>
-                <View style={styles.modelNameWrapperView}>
-                  <Text style={styles.modelNameView}>Normal</Text>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={styles.modelButtonView}
-                onPress={() => this.modelButtonPressed("Face")}
-              >
-                <View
-                  style={
-                    activeModelName == "Face"
-                      ? [styles.modelIconBG, styles.activeModelView]
-                      : styles.modelIconBG
-                  }
-                >
-                  <Icon.Ionicons name="ios-happy" size={28} color="#000" />
-                </View>
-                <View style={styles.modelNameWrapperView}>
-                  <Text style={styles.modelNameView}>Face</Text>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={[styles.modelButtonView, styles.shiftedButtonView]}
-                onPress={() => this.modelButtonPressed("Color")}
-              >
-                <View
-                  style={
-                    activeModelName == "Color"
-                      ? [styles.modelIconBG, styles.activeModelView]
-                      : styles.modelIconBG
-                  }
-                >
-                  <Icon.Ionicons
-                    name="ios-color-palette"
-                    size={28}
-                    color="#000"
+              <View style={styles.cameraViewWrapper}>
+                <View style={styles.cameraBorderView}>
+                  <Camera
+                    style={styles.cameraView}
+                    ref={ref => {
+                      this.camera = ref;
+                    }}
+                    type={this.state.type}
                   />
                 </View>
-                <View style={styles.modelNameWrapperView}>
-                  <Text style={styles.modelNameView}>Color</Text>
-                </View>
+                <Text style={styles.helperText}>Snap your photo</Text>
+                <Text style={styles.subtitle}>
+                  Understanding the world around you was never been easier. Try
+                  Random Photo
+                </Text>
+              </View>
+
+              <View style={styles.modelBarView}>
+                <TouchableOpacity
+                  style={[styles.modelButtonView]}
+                  onPress={() => this.modelButtonPressed("Basic")}
+                >
+                  <View
+                    style={
+                      activeModelName == "Basic"
+                        ? [styles.modelIconBG, styles.activeModelView]
+                        : styles.modelIconBG
+                    }
+                  >
+                    <Icon.Ionicons name="ios-apps" size={28} color="#000" />
+                  </View>
+                  <View style={styles.modelNameWrapperView}>
+                    <Text style={styles.modelNameView}>Normal</Text>
+                  </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.modelButtonView}
+                  onPress={() => this.modelButtonPressed("Face")}
+                >
+                  <View
+                    style={
+                      activeModelName == "Face"
+                        ? [styles.modelIconBG, styles.activeModelView]
+                        : styles.modelIconBG
+                    }
+                  >
+                    <Icon.Ionicons name="ios-happy" size={28} color="#000" />
+                  </View>
+                  <View style={styles.modelNameWrapperView}>
+                    <Text style={styles.modelNameView}>Face</Text>
+                  </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={[styles.modelButtonView]}
+                  onPress={() => this.modelButtonPressed("Color")}
+                >
+                  <View
+                    style={
+                      activeModelName == "Color"
+                        ? [styles.modelIconBG, styles.activeModelView]
+                        : styles.modelIconBG
+                    }
+                  >
+                    <Icon.Ionicons
+                      name="ios-color-palette"
+                      size={28}
+                      color="#000"
+                    />
+                  </View>
+                  <View style={styles.modelNameWrapperView}>
+                    <Text style={styles.modelNameView}>Color</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+
+              <TouchableOpacity
+                style={styles.discoverButtonView}
+                onPress={() => this.objectDetection(activeModelName)}
+              >
+                <Text style={styles.discoverButtonTextView}>Discover</Text>
               </TouchableOpacity>
             </View>
-
-            <TouchableOpacity
-              style={styles.discoverButtonView}
-              onPress={this.objectDetection}
-            >
-              <Text style={styles.discoverButtonTextView}>Discover</Text>
-            </TouchableOpacity>
+            {isPredictionsViewVisible && (
+              <View style={styles.overlay}>
+                <View style={styles.popupView}>
+                  <View style={styles.popupHeaderView}>
+                    <Text style={styles.popupTitle}>We've got 🙈</Text>
+                    <TouchableOpacity
+                      style={styles.closeDialog}
+                      onPress={() => this.closeDialog()}
+                    >
+                      <Text style={styles.closeDialogTextView}>Done</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.popupBodyView}>
+                    {activeModelName === "Basic" && (
+                      <FlatList
+                        data={predictions.slice(0, 10).map(prediction => ({
+                          key: `${prediction.name} ${prediction.value}`,
+                          name: prediction.name,
+                          confidence:
+                            (Math.round(prediction.value * 100) / 100) * 100
+                        }))}
+                        renderItem={({ item }) => (
+                          <View style={styles.listItemView}>
+                            <View
+                              style={[
+                                styles.iconBG,
+                                {
+                                  backgroundColor: `${randomColor.bgColor}`
+                                }
+                              ]}
+                            >
+                              <Icon.Ionicons
+                                name="ios-image"
+                                size={28}
+                                color={`${randomColor.foreGroundColor}`}
+                              />
+                            </View>
+                            <View style={styles.data}>
+                              <Text style={styles.predictionName}>
+                                {item.name}
+                              </Text>
+                              <Text style={styles.predictionPercentage}>
+                                {item.confidence}%
+                              </Text>
+                            </View>
+                          </View>
+                        )}
+                      />
+                    )}
+                    {activeModelName === "Face" && (
+                      <View style={styles.faceDetectionBody}>
+                        <View style={styles.detection}>
+                          <View style={styles.faceDetectionBG}>
+                            <Text style={styles.predictionName}>
+                              {
+                                predictions.data.face.age_appearance.concepts[0]
+                                  .name
+                              }
+                            </Text>
+                          </View>
+                          <Text style={styles.captionTextView}>
+                            Possible Age
+                          </Text>
+                        </View>
+                        <View style={styles.seperator} />
+                        <View style={styles.detection}>
+                          <View style={styles.faceDetectionBG}>
+                            <Text style={styles.predictionName}>
+                              {predictions.data.face.age_appearance.concepts[0]
+                                .name === "masculine"
+                                ? "👨"
+                                : "👩"}
+                            </Text>
+                          </View>
+                          <Text style={styles.captionTextView}>
+                            Possible Gender
+                          </Text>
+                        </View>
+                      </View>
+                    )}
+                  </View>
+                </View>
+              </View>
+            )}
           </View>
         )
       );
--- styles.js
+++ styles.js
@@ -1,12 +1,20 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+let deviceWidth = Dimensions.get("window").width;
+let deviceHeight = Dimensions.get("window").height;
 
 export default StyleSheet.create({
   wrapper: {
     flex: 1,
+    position: "relative"
+  },
+  homeView: {
+    flex: 1,
     backgroundColor: "#000",
+    paddingHorizontal: 32,
+    paddingVertical: 24,
     alignItems: "center",
-    justifyContent: "space-between",
-    padding: 32
+    justifyContent: "space-between"
   },
   header: {
     flexDirection: "row",
@@ -23,6 +31,14 @@ export default StyleSheet.create({
   muted: {
     color: "#999"
   },
+  switchCameraViewBG: {
+    backgroundColor: "rgba(255,255,255,0.2)",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 40,
+    height: 40,
+    borderRadius: 20
+  },
   cameraViewWrapper: {
     justifyContent: "center",
     alignItems: "center"
@@ -31,21 +47,29 @@ export default StyleSheet.create({
     borderWidth: 2,
     borderStyle: "dotted",
     borderColor: "#fff",
-    width: 240,
-    height: 240,
-    borderRadius: 120
+    width: 250,
+    height: 250,
+    borderRadius: 125
   },
   cameraView: {
     width: "100%",
     height: "100%",
-    borderRadius: 120,
+    borderRadius: 125,
     overflow: "hidden"
   },
   helperText: {
     fontFamily: "maison-neue-demi",
     color: "#fff",
     fontSize: 24,
-    marginVertical: 24
+    marginTop: 20
+  },
+  subtitle: {
+    fontFamily: "maison-neue-demi",
+    color: "rgba(255,255,255,0.6)",
+    fontSize: 14,
+    textAlign: "center",
+    lineHeight: 20,
+    marginTop: 10
   },
   discoverButtonView: {
     width: "100%",
@@ -74,9 +98,7 @@ export default StyleSheet.create({
     alignItems: "center",
     position: "relative"
   },
-  shiftedButtonView: {
-    marginBottom: 64
-  },
+
   modelIconBG: {
     backgroundColor: "#BFBFBF",
     width: 64,
@@ -101,6 +123,108 @@ export default StyleSheet.create({
     fontSize: 12
   },
   activeModelView: {
-    backgroundColor: "#F8AE23"
+    backgroundColor: "#FF9C00"
+  },
+  overlay: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0,0,0,0.8)",
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  popupView: {
+    width: deviceWidth,
+    height: deviceHeight - 120,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    backgroundColor: "#fff"
+  },
+  popupHeaderView: {
+    width: "100%",
+    height: 80,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e6e6e6"
+  },
+  popupTitle: {
+    fontFamily: "maison-neue-bold",
+    fontSize: 20
+  },
+  closeDialogTextView: {
+    fontFamily: "maison-neue-demi",
+    fontSize: 14,
+    color: "#999"
+  },
+  popupBodyView: { width: "100%", paddingBottom: 80 },
+  listItemView: {
+    width: "100%",
+    height: 100,
+    justifyContent: "flex-start",
+    paddingHorizontal: 32,
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  data: {
+    height: "100%",
+    justifyContent: "center",
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e6e6e6"
+  },
+  predictionName: {
+    fontFamily: "maison-neue-bold",
+    fontSize: 20
+  },
+  predictionPercentage: {
+    fontFamily: "maison-neue-demi",
+    marginTop: 6,
+    color: "#666"
+  },
+  iconBG: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 16
+  },
+  faceDetectionBody: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    flexDirection: "column"
+  },
+  detection: {
+    alignItems: "center",
+    marginVertical: 20
+  },
+  faceDetectionBG: {
+    width: 80,
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    borderRadius: 40,
+    backgroundColor: "#e6e6e6"
+  },
+  seperator: {
+    width: deviceWidth - 40,
+    height: 1,
+    backgroundColor: "#e6e6e6",
+    marginVertical: 32
+  },
+  captionTextView: {
+    fontSize: 14,
+    color: "#666",
+    fontFamily: "maison-neue-demi"
   }
 });
